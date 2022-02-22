61481cfc35fdf638d982cd9f9e51f917fceab35b
Lars Boß <lars.boss@prodyna.com>
2022-02-22 16:20:11 +0100

    working proxy

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const needle = require("needle");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000;
+const CORS_HEADER = process.env.CORS_HEADER || "";
+const BASE_URL = process.env.BASE_URL || "http://localhost:8080"
+
+const app = express();
+//Use Bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//Enable cors
+app.use(
+  cors({
+    origin: CORS_HEADER,
+  })
+);
+app.all("/*", async (req, res) => {
+  console.log(BASE_URL + req.url);
+  try {
+    const apiRes = await needle(req.method, BASE_URL + req.url, req.body);
+    res.status(200).json(apiRes.body);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
