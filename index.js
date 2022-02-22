9af1cb65f1f33a7ac2e5bbb44c979cddac82e51c
Lars Boß <lars.boss@prodyna.com>
2022-02-22 16:22:58 +0100

    added comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const needle = require("needle");
 const bodyParser = require("body-parser");
-
+//Set variables
 const PORT = process.env.PORT || 5000;
 const CORS_HEADER = process.env.CORS_HEADER || "";
 const BASE_URL = process.env.BASE_URL || "http://localhost:8080"
@@ -12,14 +12,13 @@ const app = express();
 //Use Bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//Enable cors
+//Set CORS-Header
 app.use(
   cors({
     origin: CORS_HEADER,
   })
 );
 app.all("/*", async (req, res) => {
-  console.log(BASE_URL + req.url);
   try {
     const apiRes = await needle(req.method, BASE_URL + req.url, req.body);
     res.status(200).json(apiRes.body);
