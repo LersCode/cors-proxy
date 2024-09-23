a63fce59b5601e5d9b6708075168be36d8648620
Lars B <65869183+LersCode@users.noreply.github.com>
2024-09-23 17:36:01 +0200

    Refactor some keys and add comments

--- index.js
+++ index.js
@@ -7,28 +7,36 @@ const bodyParser = require("body-parser");
 //Set variables
 const PORT = process.env.PORT || 5000;
 const CORS_HEADER = process.env.CORS_HEADER || "";
-const BASE_URL = process.env.BASE_URL || "http://localhost:8080";
+const BASE_URL = process.env.BASE_URLL || "http://localhost:8080";
 const ENABLE_LOGGING = process.env.ENABLE_LOGGING == "true" || false;
 
 const app = express();
+
 //Use Bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 //Set CORS-Header
 app.use(
   cors({
     origin: CORS_HEADER,
   })
 );
+/**
+ * INTERCEPTORS
+ */
+
+// All requests
 app.all("/*", async (req, res) => {
-  logging(req.url);
+  logging("request_url", req.url);
   try {
     const apiRes = await needle(req.method, BASE_URL + req.url, req.body);
-    res.status(200).json(apiRes.body);
+    res.status(200).send(apiRes.body);
     logging("Proxy-Status 200");
   } catch (error) {
     res.status(500).json(error);
     logging("Proxy-Status 500");
+    logging("Proxy-Error", error);
   }
 });
 
